c935b34bb4b72cc20d564ee601034e8174857813
TewPingz <38973872+TewPingz@users.noreply.github.com>
2025-03-16 04:19:50 +1100

    collectible load from server

--- src/collectibles.ts
+++ src/collectibles.ts
@@ -1,4 +1,5 @@
 export interface Collectible {
+    id: string;
     name: string;
     image: string;
     position: { top: string; left: string };
@@ -12,11 +13,11 @@ export interface Collectible {
   import c6 from "./assets/collectibles/collectible_6.png";
   
   export const collectibles: Collectible[] = [
-    { name: "Item 1", image: c1, position: { top: "47%", left: "43%" } },
-    { name: "Item 2", image: c2, position: { top: "44%", left: "47%" } },
-    { name: "Item 3", image: c3, position: { top: "35%", left: "53%" } },
-    { name: "Item 4", image: c4, position: { top: "65%", left: "46%" } },
-    { name: "Item 5", image: c5, position: { top: "57%", left: "54%" } },
-    { name: "Item 6", image: c6, position: { top: "53%", left: "58%" } },
+    { id: "collectible_1", name: "Item 1", image: c1, position: { top: "47%", left: "43%" } },
+    { id: "collectible_2", name: "Item 2", image: c2, position: { top: "44%", left: "47%" } },
+    { id: "collectible_3", name: "Item 3", image: c3, position: { top: "35%", left: "53%" } },
+    { id: "collectible_4", name: "Item 4", image: c4, position: { top: "65%", left: "46%" } },
+    { id: "collectible_5", name: "Item 5", image: c5, position: { top: "57%", left: "54%" } },
+    { id: "collectible_6", name: "Item 6", image: c6, position: { top: "53%", left: "58%" } },
   ];
   
