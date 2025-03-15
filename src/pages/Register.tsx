f4bf1e90e1672bc35516494497b9dbc781021820
amaiable <atmai2002@gmail.com>
2025-03-15 16:38:13 +1100

    rebase

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -4,32 +4,49 @@ import PrettyButton from '../PrettyButton'
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import coffeeShopImage from '../assets/coffee_shop_title.png';
+import restClient from "../utils/rest.util";
 
 function Register() {
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
-  const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
 
   const navigate = useNavigate();
 
-  const register = (e: any) => {
+  const register = async (e: any) => {
     e.preventDefault();
+
+    const request = await restClient.post('/auth/register', {
+      data: {
+        username: username,
+        password: password
+      }
+    });
+
+    if (!request.success) {
+      // setError(request.data)  // TODO: Handle error
+      return
+    }
+
+    localStorage.setItem("accessToken", request.data.token);
+    localStorage.setItem("username", JSON.stringify(request.data.username));
+
     navigate("/prompt");
   }
-  
+
   return (
     <div style={{ backgroundColor: '#472200' }} className="bg-orange-900 min-h-screen flex flex-col
                     items-center justify-center"
     >
-    <img 
-          src={coffeeShopImage} 
-          alt="Coffee Shop Logo" 
-          className="w-auto h-30" // You can adjust the size as needed
-        />
+      <img
+        src={coffeeShopImage}
+        alt="Coffee Shop Logo"
+        className="w-auto h-30" // You can adjust the size as needed
+      />
       <div className="bg-white p-6 rounded-md shadow-md w-full max-w-lg">
         <h2 style={{ color: '#472200' }} className="text-4xl font-bold mb-4 text-center">Register</h2>
 
-        <Box 
+        <Box
           component="form"
           sx={{
             display: 'flex',
@@ -38,34 +55,34 @@ function Register() {
           }}
           noValidate
           onSubmit={register}
-        >   
-          <TextField 
-            id="outlined-basic" 
-            label="Name" 
-            variant="outlined" 
-            value={name}
-            onChange={e => setName(e.target.value)}
-          />   
+        >
+          <TextField
+            id="outlined-basic"
+            label="Username"
+            variant="outlined"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+          />
 
-          <TextField 
-            id="outlined-basic" 
-            label="Password" 
-            variant="outlined" 
+          <TextField
+            id="outlined-basic"
+            label="Password"
+            variant="outlined"
             value={password}
-            type="password" 
+            type="password"
             onChange={e => setPassword(e.target.value)}
           />
 
-          <TextField 
-            id="outlined-basic" 
-            label="Confirm Password" 
-            variant="outlined" 
+          <TextField
+            id="outlined-basic"
+            label="Confirm Password"
+            variant="outlined"
             value={confirmPassword}
-            type="password" 
+            type="password"
             onChange={e => setConfirmPassword(e.target.value)}
           />
 
-          <PrettyButton 
+          <PrettyButton
             type="submit"
           >
             Register
