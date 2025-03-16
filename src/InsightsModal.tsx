2ea29a1a48caf66df3d5ac00774bef99892c5f33
TewPingz <38973872+TewPingz@users.noreply.github.com>
2025-03-16 14:32:33 +1100

    added insight human readable format

--- src/InsightsModal.tsx
+++ src/InsightsModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Modal.css';
 import './PrettyList.css';
 import signFrameImage from './assets/cafe_sign_frame.png';
@@ -11,14 +11,27 @@ interface ModalProps {
   userLeaderboard: any;
 }
 
+const convertSecondstoTime = (input_seconds) => {
+  let hours = Math.floor(input_seconds / 3600);
+  let minutes = Math.floor((input_seconds - (hours * 3600)) / 60);
+  let seconds = input_seconds - (hours * 3600) - (minutes * 60);
+
+  return hours.toString().padStart(2, '0') + ':' +
+    minutes.toString().padStart(2, '0') + ':' +
+    seconds.toString().padStart(2, '0');
+}
+
 const InsightsModal: React.FC<ModalProps> = ({ isOpen, onClose, totalFocusTime, userLeaderboard }) => {
-  console.log(totalFocusTime)
-  // const [users, setUsers] = useState<{ username: string, hours: number }[]>([]);
-  const users: any = [];
-  for (let i = 0; i < userLeaderboard.length; i++) {
-    users.push({ username: userLeaderboard[i].username, hours: Math.round(userLeaderboard[i].focus_time_seconds / 3600) });
-  }
-  const sortedUsers = [...users].sort((a, b) => b.hours - a.hours);
+  const [users, setUsers] = useState<{ username: string, seconds: number, time: string }[]>([]);
+
+  useEffect(() => {
+    const track = []
+    for (let x of userLeaderboard) {
+      track.push({ username: x.username, seconds: x.focus_time_seconds, time: convertSecondstoTime(x.focus_time_seconds) });
+    }
+    const sortedUsers = [...users].sort((a, b) => b.seconds - a.seconds);
+    setUsers(sortedUsers)
+  }, [userLeaderboard])
 
   // Get from backend the users array and sort in descending order comparing hours
   if (!isOpen) return null;
@@ -27,12 +40,12 @@ const InsightsModal: React.FC<ModalProps> = ({ isOpen, onClose, totalFocusTime, 
     <div className="section full-height">
       <div className="modal" onClick={onClose}>
         <div className="relative h-full" onClick={(e) => e.stopPropagation()}>
-          <img 
-            src={signFrameImage} 
-            alt="Modal Frame" 
+          <img
+            src={signFrameImage}
+            alt="Modal Frame"
             className="h-full"
           />
-          
+
           <div className="absolute top-[38%] left-[24%] w-[55%] h-[40%] overflow-auto">
             <StyledText>
               Total Time Worked:
@@ -55,7 +68,7 @@ const InsightsModal: React.FC<ModalProps> = ({ isOpen, onClose, totalFocusTime, 
               Current Leaderboard:
             </StyledText>
             <ul style={{ textAlign: "center", padding: 0, listStyle: "none" }}>
-              {sortedUsers.map((user, index) => (
+              {users.map((user, index) => (
                 <li
                   key={user.username}
                   style={{
@@ -65,7 +78,7 @@ const InsightsModal: React.FC<ModalProps> = ({ isOpen, onClose, totalFocusTime, 
                     fontFamily: '"Gloria Hallelujah", cursive',
                   }}
                 >
-                  {index + 1}. {user.username} - {user.hours} hours
+                  {index + 1}. {user.username} - {user.time} hours
                 </li>
               ))}
             </ul>
